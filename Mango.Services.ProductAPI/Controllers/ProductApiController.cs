aa419a46367d5c6ccc5e632028bab8fa65cf7d05
AlexAR <moltron_nano@hotmail.com>
2023-05-08 10:51:22 -0600

    feat: add product controller endpoint and volume to docker

---
+++ Mango.Services.ProductAPI/Controllers/ProductApiController.cs
@@ -0,0 +1,41 @@
+﻿using Mango.Services.ProductAPI.Models.Dtos;
+using Mango.Services.ProductAPI.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.ProductAPI.Controllers
+{
+    [ApiController]
+    [Route("api/")]
+    public class ProductApiController : ControllerBase
+    {
+        protected ResponseDto _response;
+        private IProductRepository _productRepository;
+
+        public ProductApiController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+            this._response = new ResponseDto();
+        }
+
+        [HttpGet]
+        public async Task<object> Get()
+        {
+            try
+            {
+
+                IEnumerable<ProductDto> productDtos = await _productRepository.GetProducts();
+                _response.isSuccess = true;
+                _response.Result = productDtos;
+            }
+            catch (Exception ex)
+            {
+
+                _response.isSuccess = false;
+                _response.ErrorMessages
+                    = new List<string> { ex.Message };
+            }
+            return _response;
+        }
+
+    }
+}
