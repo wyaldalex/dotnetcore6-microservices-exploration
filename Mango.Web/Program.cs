66f8d790aab2fc2634bdddbbe2ce6ed5fe0be4c9
Alexander Alvarenga <moltron_nano@hotmail.com>
2023-05-15 12:08:10 -0600

    feat: add generic api request and product service at front

--- Mango.Web/Program.cs
+++ Mango.Web/Program.cs
@@ -1,7 +1,19 @@
+using Mango.Web;
+using Mango.Web.Services;
+using Mango.Web.Services.IServices;
+
 var builder = WebApplication.CreateBuilder(args);
 
+//change related to .net core 6
+// source: https://gavilan.blog/2021/08/19/getting-the-iconfiguration-in-the-program-class-asp-net-core-6/
+var configuration = builder.Configuration;
+
 // Add services to the container.
+// .Net 6 merges Startup and Program classes 
 builder.Services.AddControllersWithViews();
+builder.Services.AddHttpClient<IProductService, ProductService>();
+builder.Services.AddScoped<IProductService, ProductService>();
+SD.ProductAPIBase = configuration["ServiceUrls:ProductAPI"];
 
 var app = builder.Build();
 
